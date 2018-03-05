2cc2c9236ad6c5ad98098c41b45750300450bb90
Victor Seva <vseva@debian.org>
2018-03-05 14:33:06 +0100

    fix warning for fallthrough
    > dbd/postgresql/connection.c:57:5: warning: this statement may fall through [-Wimplicit-fallthrough=]
    >   if (lua_isnil(L, 5) == 0)
    >      ^
    > dbd/postgresql/connection.c:68:5: note: here
    >      case 4:
    >      ^~~~
    > dbd/postgresql/connection.c:69:5: warning: this statement may fall through [-Wimplicit-fallthrough=]
    >   if (lua_isnil(L, 4) == 0)
    >      ^
    > dbd/postgresql/connection.c:71:5: note: here
    >      case 3:
    >      ^~~~
    > dbd/postgresql/connection.c:72:5: warning: this statement may fall through [-Wimplicit-fallthrough=]
    >   if (lua_isnil(L, 3) == 0)
    >      ^
    > dbd/postgresql/connection.c:74:5: note: here
    >      case 2:
    >      ^~~~
    > dbd/postgresql/connection.c:75:5: warning: this statement may fall through [-Wimplicit-fallthrough=]
    >   if (lua_isnil(L, 2) == 0)
    >      ^
    > dbd/postgresql/connection.c:77:5: note: here
    >      case 1:
    >      ^~~~
    
    > dbd/mysql/connection.c: In function 'connection_new':
    > dbd/mysql/connection.c:25:5: warning: this statement may fall through [-Wimplicit-fallthrough=]
    >   if (lua_isnil(L, 5) == 0)
    >      ^
    > dbd/mysql/connection.c:27:5: note: here
    >      case 4:
    >      ^~~~
    > dbd/mysql/connection.c:30:5: warning: this statement may fall through [-Wimplicit-fallthrough=]
    >   if (host != NULL) {
    >      ^
    > dbd/mysql/connection.c:36:5: note: here
    >      case 3:
    >      ^~~~
    > dbd/mysql/connection.c:37:5: warning: this statement may fall through [-Wimplicit-fallthrough=]
    >   if (lua_isnil(L, 3) == 0)
    >      ^
    > dbd/mysql/connection.c:39:5: note: here
    >      case 2:
    >      ^~~~
    > dbd/mysql/connection.c:40:5: warning: this statement may fall through [-Wimplicit-fallthrough=]
    >   if (lua_isnil(L, 2) == 0)
    >      ^
    > dbd/mysql/connection.c:42:5: note: here
    >      case 1:
    >      ^~~~

--- dbd/postgresql/connection.c
+++ dbd/postgresql/connection.c
@@ -65,20 +65,25 @@ static int connection_new(lua_State *L) {
 		luaL_error(L, DBI_ERR_INVALID_PORT, pport);
 	    }
 	}
+    // fallthrough
     case 4: 
 	if (lua_isnil(L, 4) == 0) 
 	    host = luaL_checkstring(L, 4);
+    // fallthrough
     case 3:
 	if (lua_isnil(L, 3) == 0) 
 	    password = luaL_checkstring(L, 3);
+    // fallthrough
     case 2:
 	if (lua_isnil(L, 2) == 0) 
 	    user = luaL_checkstring(L, 2);
+    // fallthrough
     case 1:
 	/*
 	 * db is the only mandatory parameter
 	 */
 	db = luaL_checkstring(L, 1);
+    // fallthrough
     }
 
     conn = (connection_t *)lua_newuserdata(L, sizeof(connection_t));
